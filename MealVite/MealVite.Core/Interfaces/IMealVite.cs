9f433b0c684c384f95330b39e80b28c608c412c0
capitaljps <janpatricksacay@gmail.com>
2015-08-15 20:22:41 +0800

    Revert "*Added functions in MealViteRepo"
    This reverts commit 2ddfe759922d75dd35ca23955590f7241048abb4.

--- MealVite/MealVite.Core/Interfaces/IMealVite.cs
+++ MealVite/MealVite.Core/Interfaces/IMealVite.cs
@@ -11,11 +11,5 @@ namespace MealVite.Core.Interfaces
     public interface IMealVite : IRepository<Mealvite>
     {
         void Reserve(int mealviteId, int userId);
-        void CancelReservation(int mealviteId, int userId);
-        void CancelMealvite(int mealviteId, int userId, string reason);
-        void MoveMealvite(int mealviteId, int userId, DateTime date);
-        void RateAndComment(int mealviteId, int userId, int rate, string comment);
-        double CalculateTotal(int mealviteId);
-        int GetAtendeesCount(int mealviteId);
     }
 }
